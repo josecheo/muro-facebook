c1a126e2de997647d32065e8878845cb88769391
Jose Alvarez <joseche0.ja@gmail.com>
2020-02-08 02:29:11 -0500

    nuevo post

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,9 @@ import React from "react";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faXing } from "@fortawesome/free-brands-svg-icons";
 import "./styles/Post.css";
+import avatar from "../img/avatar.png";
+import galeria from "../img/galeria.svg";
+import etiquetar from "../img/etiquetar.png";
 
 class Post extends React.Component {
   render() {
@@ -9,8 +12,25 @@ class Post extends React.Component {
       <React.Fragment>
         <div className="Post">
           <div className="Post__card-header">Crear publicación </div>
-          <div className="Post__avatar"> </div>
-          <div className="Post_textarea text-area"></div>
+          <img className="Post__avatar" src={avatar} alt="Logo"></img>
+
+          <textarea
+            className="Post_textarea"
+            placeholder="¿Que estas pensando, Jose?"
+          ></textarea>
+
+          <div className="Post__buttons--container">
+            <div className="upload_photo--container">
+              <img src={galeria} alt="subir media"></img>
+              <span>Foto/video</span>
+            </div>
+
+            <div className="upload_photo--container">
+              <img src={etiquetar} alt="etiquetar amigos"></img>
+              <span>Etiquetar</span>
+            </div>
+          </div>
+          <div className="button">Publicar</div>
         </div>
       </React.Fragment>
     );
